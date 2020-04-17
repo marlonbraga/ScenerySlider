0b63688a629747e471be5bde77ebbc49eed02f84
Marlon Braga <marlon@marlonbraga.com.br>
2020-04-17 16:01:15 -0300

    Install EntityFramework and create a Media class

---
+++ ScenerySlider/Models/Media.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScenerySlider.Models {
+	public abstract class Media {
+		protected int id;
+		protected string name;
+		protected string fileLocate;
+		protected string description;
+
+		protected abstract void Play();
+		protected abstract void Stop();
+	}
+}
