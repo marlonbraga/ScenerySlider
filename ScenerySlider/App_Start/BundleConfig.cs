4c8a668111f470c8354d2be7a12a090b7be64bfe
Marlon Braga <marlon@marlonbraga.com.br>
2020-04-29 20:08:40 -0300

    Fix Bootstrap reference
    Tried to make a local reference to css bootstrap file. But the icons of
    glyphicon doesn't work. Font Awesome 5 was  also referenced but it
    works well.

--- ScenerySlider/App_Start/BundleConfig.cs
+++ ScenerySlider/App_Start/BundleConfig.cs
@@ -1,34 +1,32 @@
-﻿using System.Web;
+﻿using Microsoft.Ajax.Utilities;
+using System.Web;
 using System.Web.Optimization;
 
 namespace ScenerySlider {
 	public class BundleConfig {
-		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+		const string cdnBootstrapCss = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css";
+		
 		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.UseCdn = true;
+
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
-
+					"~/Scripts/jquery-{version}.js"));
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate*"));
-
-			// Use the development version of Modernizr to develop with and learn from. Then, when you're
-			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+					"~/Scripts/jquery.validate*"));
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
-
+					"~/Scripts/modernizr-*"));
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
-
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
-
+					"~/Scripts/bootstrap.js"));
+			bundles.Add(new StyleBundle("~/Content/css",cdnBootstrapCss).Include(
+					"~/Content/bootstrap.css"));
+			bundles.Add(new ScriptBundle("~/bundles/icons").Include(
+					"~/Scripts/icons/a076d05399.js"));
 			bundles.Add(new StyleBundle("~/Content/tour").Include(
-					  "~/Content/TourFrame.css"));
-			//bundles.Add(new ScriptBundle("~/bundles/tour").Include(
-			//		  "~/Scripts/TourFrame/TourDragBackground.js"));
+					"~/Content/TourFrame.css"));
 			bundles.Add(new ScriptBundle("~/bundles/tour").Include(
-					  "~/Scripts/TourFrame/TourRevealBackground.js"));
+					"~/Scripts/TourFrame/TourDragBackground.js"));
+			//bundles.Add(new ScriptBundle("~/bundles/tour").Include(
+			//		  "~/Scripts/TourFrame/TourRevealBackground.js"));
 
 			BundleTable.EnableOptimizations = true;
 
